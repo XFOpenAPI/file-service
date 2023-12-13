c4b246df152d71dfdd5c2292b5bd1171c511165a
XFOpenAPI <150761801+XFOpenAPI@users.noreply.github.com>
2023-12-13 20:53:07 +0800

    Delete openapi-rapi/src/test directory

--- openapi-rapi/src/test/java/com/xforceplus/openapi/api/infrastructure/util/DemoTest.java
+++
@@ -1,8 +0,0 @@
-package com.xforceplus.openapi.api.infrastructure.util;
-
-public class DemoTest {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
-
